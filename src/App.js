842b0e9fc5b6f36cff0b1f484adfa31846a2b058
mr_noppo <takizawa20383461@gmail.com>
2024-05-07 02:44:35 +0900

    再読み込み禁止を追加

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form"
 import { useState } from "react"
+import { useEffect } from "react";
 
 
 export default function App() {
@@ -11,7 +12,21 @@ export default function App() {
     formState: { errors },
   } = useForm({defaultValues: {example: 'aaaaaa'}})
 
+  useEffect(() => {
+    const handleBeforeUnload = event => {
+        // メッセージはほとんどのブラウザでは無視されますが、設定する必要があります
+        event.preventDefault() // (1)
+        event.returnValue = '' // (2)
+    }
 
+    // イベントリスナーを登録
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    // クリーンアップ関数
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    }
+  }) 
   const example = watch('example')
   const onSubmit = (data) => console.log(data)
 
@@ -24,7 +39,6 @@ export default function App() {
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
       <input {...register("example", { disabled: count===0?true:false })} />
-
 
       {/* include validation with required or other standard HTML validation rules */}
       <input defaultValue="test" {...register("exampleRequired", { minLength:{ value:4, message:'Min Length is 4'}  })} />
